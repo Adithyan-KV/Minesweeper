be6b7b1c773203b8c7bfabee466951040fd8fd4d
Adithyan-KV <adithyankv007@gmail.com>
2020-08-05 02:20:25 +0530

    Added click to reveal functionality
     -The cells of the minefield now can be clicked to reveal the
      underlying stuff, whether it be empty, a bomb count or the bomb
     -The cells were styled for different numbers of neighbor count and
      bomb.

--- minesweeper.js
+++ minesweeper.js
@@ -22,125 +22,14 @@ document.addEventListener('DOMContentLoaded',()=>{
     }
     const shuffledCells = shuffle(combinedCells);
 
-    //given a cell in the shuffled array, computes the number of neighbouring
-    //cells which have a bomb
-    function neigboringBombCount(cell){
-        let cellIndex = Number(cell.getAttribute('id'));
-        // alert(cellIndex);
-        let count = 0;
-        //Indices of all the corners
-        corners=[0,(width-1),width*(width-1),width*width-1];
-        //indices of neighboring cells
-        const top=cellIndex-width;
-        const topLeft=(cellIndex-width)-1;
-        const topRight=(cellIndex-width)+1;
-        const bottom=(cellIndex+width);
-        const bottomLeft=(cellIndex+width)-1;
-        const bottomRight=(cellIndex+width)+1;
-        const right=cellIndex+1;
-        const left=cellIndex-1;
-        console.log(`cell:${cellIndex}:${[topLeft,top,topRight,left,right,bottomLeft,bottom,bottomRight]}`);
-        //if cell is not on left or right edge
-        if(cellIndex%width!=0&&cellIndex%width!=9){
-            //if cell is not on top or bottom edge
-            if(cellIndex<90&&cellIndex>9){
-                //indices of all the neighboring cells
-                const neighborCells = [topLeft,top,topRight,left,right,bottomLeft,bottom,bottomRight];
-                for (let j=0;j< neighborCells.length;j++){
-                    if(shuffledCells[neighborCells[j]]==='bomb'){
-                        count++;
-                    }
-                }
-            }
-            //if cell is in bottom row
-            else if(cellIndex>90&&!corners.includes(cellIndex)){
-                const neighborCells = [left,right,topLeft,top,topRight];
-                for (let j=0;j< neighborCells.length;j++){
-                    if(shuffledCells[neighborCells[j]]==='bomb'){
-                        count++;
-                    }
-                }
-            }
-            else if(cellIndex<10&&!corners.includes(cellIndex)){
-                const neighborCells = [left,right,bottomLeft,bottom,bottomRight];
-                for (let j=0;j< neighborCells.length;j++){
-                    if(shuffledCells[neighborCells[j]]==='bomb'){
-                        count++;
-                    }
-                }
-            }
-        }
-        //if cell is on left edge but not a corner
-        else if(cellIndex%width===0&& !corners.includes(cellIndex)){
-            const neighborCells = [top,topRight,right,bottom,bottomRight];
-            for (let j=0;j< neighborCells.length;j++){
-                if(shuffledCells[neighborCells[j]]==='bomb'){
-                    count++;
-                }
-            }
-        }
-        //if cell is on right edge but not a corner
-        else if(cellIndex%width===9&& !corners.includes(cellIndex)){
-            const neighborCells = [topLeft,top,left,bottomLeft,bottom];
-            for (let j=0;j< neighborCells.length;j++){
-                if(shuffledCells[neighborCells[j]]==='bomb'){
-                    count++;
-                }
-            }
-        }
-        else if(cellIndex===0){
-            const neighborCells = [right,bottomRight,bottom];
-            for (let j=0;j< neighborCells.length;j++){
-                if(shuffledCells[neighborCells[j]]==='bomb'){
-                    count++;
-                }
-            }
-        }
-        else if(cellIndex===(width-1)){
-            const neighborCells = [left,bottomLeft,bottom];
-            for (let j=0;j< neighborCells.length;j++){
-                if(shuffledCells[neighborCells[j]]==='bomb'){
-                    count++;
-                }
-            }
-        }
-        else if(cellIndex===(width*(width-1))){
-            const neighborCells = [right,topRight,top];
-            for (let j=0;j< neighborCells.length;j++){
-                if(shuffledCells[neighborCells[j]]==='bomb'){
-                    count++;
-                }
-            }
-        }
-        else{
-            const neighborCells = [left,topLeft,top];
-            for (let j=0;j< neighborCells.length;j++){
-                if(shuffledCells[neighborCells[j]]==='bomb'){
-                    count++;
-                }
-            }
-        };
-        //
-        // function checkCells(neighborCells){
-        //     for (let j=0;j< neighborCells.length;j++){
-        //         if(shuffledCells[neighborCells[j]]==='bomb'){
-        //             count++;
-        //         }
-        //     }
-        // };
-
-        return count;
-    };
-
-
-
     //add the 100 cells to the minefield
     for(let i=0;i<width*width;i++){
         let cell = document.createElement('div');
         cell.classList.add('cell');
         cell.setAttribute('id',i);
+        cell.setAttribute('count',neigboringBombCount(cell));
         // cell.innerHTML=i;
-        cell.innerHTML=neigboringBombCount(cell);
+        // cell.innerHTML=neigboringBombCount(cell);
 
         //setting colors in a checkerboard pattern
         if (i%20<10){
@@ -166,6 +55,89 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     };
 
+    //given a cell in the shuffled array, computes the number of neighbouring
+    //cells which have a bomb
+    function neigboringBombCount(cell){
+        let cellIndex = Number(cell.getAttribute('id'));
+        // alert(cellIndex);
+        let count = 0;
+        //Indices of all the corners
+        corners=[0,(width-1),width*(width-1),width*width-1];
+        //indices of neighboring cells
+        const top=cellIndex-width;
+        const topLeft=(cellIndex-width)-1;
+        const topRight=(cellIndex-width)+1;
+        const bottom=(cellIndex+width);
+        const bottomLeft=(cellIndex+width)-1;
+        const bottomRight=(cellIndex+width)+1;
+        const right=cellIndex+1;
+        const left=cellIndex-1;
+
+        //if cell is not on left or right edge
+        if(cellIndex%width!=0&&cellIndex%width!=9){
+            //if cell is not on top or bottom edge
+            if(cellIndex<90&&cellIndex>9){
+                //indices of all the neighboring cells
+                const neighborCells = [topLeft,top,topRight,left,right,bottomLeft,bottom,bottomRight];
+                checkCells(neighborCells);
+            }
+            //if cell is in bottom row
+            else if(cellIndex>90&&!corners.includes(cellIndex)){
+                const neighborCells = [left,right,topLeft,top,topRight];
+                checkCells(neighborCells);
+            }
+            else if(cellIndex<10&&!corners.includes(cellIndex)){
+                const neighborCells = [left,right,bottomLeft,bottom,bottomRight];
+                checkCells(neighborCells);
+            }
+        }
+        //if cell is on left edge but not a corner
+        else if(cellIndex%width===0&& !corners.includes(cellIndex)){
+            const neighborCells = [top,topRight,right,bottom,bottomRight];
+            checkCells(neighborCells);
+        }
+        //if cell is on right edge but not a corner
+        else if(cellIndex%width===9&& !corners.includes(cellIndex)){
+            const neighborCells = [topLeft,top,left,bottomLeft,bottom];
+            checkCells(neighborCells);
+        }
+        //if cells are in the 4 corners
+        //top left
+        else if(cellIndex===0){
+            const neighborCells = [right,bottomRight,bottom];
+            checkCells(neighborCells);
+        }
+        //top right
+        else if(cellIndex===(width-1)){
+            const neighborCells = [left,bottomLeft,bottom];
+            checkCells(neighborCells);
+        }
+        //bottom left
+        else if(cellIndex===(width*(width-1))){
+            const neighborCells = [right,topRight,top];
+            checkCells(neighborCells);
+        }
+        //bottom right
+        else{
+            const neighborCells = [left,topLeft,top];
+            checkCells(neighborCells);
+        };
+
+        //checks all the neighbouring cells and tallies out a count
+        function checkCells(neighborCells){
+            for (let j=0;j< neighborCells.length;j++){
+                if(shuffledCells[neighborCells[j]]==='bomb'){
+                    count++;
+                }
+            }
+        };
+
+        return count;
+    };
+
+
+
+
     //implementing gameplay
     cells = document.querySelectorAll('.cell');
     cells.forEach((cell)=>{cell.addEventListener('click', clicked)});
@@ -173,9 +145,16 @@ document.addEventListener('DOMContentLoaded',()=>{
     //if a cell gets clicked
     function clicked(){
         //if cell has bomb end game
+        const count=Number(this.getAttribute('count'));
         if (this.classList.contains('bomb')){
-            alert('Game over');
+            this.innerHTML='&#11044;';
+            // alert('Game over');
         }
+        //if cell contains bomb display count on click
+        else if(count!=0){
+            this.innerHTML=count;
+        }
+
 
         //paints the cell as clicked
         this.classList.add('clicked');
